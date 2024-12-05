4417ae4975d7c40cb1d3a18a585b9834cb2191e1
Muhammad <mr.alimuhammadi@gmail.com>
2024-12-05 13:59:01 +0330

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: '/truth-table-solver' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: '/truth-table-solver/' })})}
