d0af5dd562814b4137ea950fb62f97a20fd4d718
Muhammad <mr.alimuhammadi@gmail.com>
2024-12-05 13:16:56 +0330

    add pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,42 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "",
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: ["/fonts/*, /favicon-512x512.png", "/favicon-192x192.png"],
+      manifest: {
+        name: "Truth Table Solver",
+        short_name: "Truth Table Solver",
+        start_url: "/",
+        description:
+          "Truth Table Solver is a web app to generate and solve truth tables for logical expressions, simplify equations, and more.",
+        icons: [
+          {
+            src: "/favicon-512x512.png",
+            type: "image/png",
+            sizes: "512x512",
+            purpose: "any",
+          },
+          {
+            src: "/favicon-192x192.png",
+            type: "image/png",
+            sizes: "192x192",
+            purpose: "any",
+          },
+        ],
+        background_color: "#f8f9fa",
+        theme_color: "#0d6efd",
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,webmanifest}"],
+      },
+    }),
+  ],
   css: {
     modules: {
       scopeBehaviour: "local", // 'global' or 'local'
